f34582821e9ff217c928225f9074b3fdb1d141bf
Lee Aplin <lee@substrakt.com>
2022-08-18 22:46:31 +0100

    UPDATE: Image procesing diagram

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from diagrams import Diagram
+from diagrams import Cluster, Diagram
+from diagrams.custom import Custom
 from diagrams.aws.database import Aurora
 from diagrams.aws.network import CloudFront
 from diagrams.aws.security import ACM
 from diagrams.aws.storage import S3
 from diagrams.aws.compute import Lambda
 
-with Diagram("Web Service", show=False):
-    Aurora("database") >> S3("media storage") >> CloudFront("caching and CDN")
+with Diagram("Tachyon", show=False):
+    wordpress = Custom("Wordpress website", "./img/wordpress.png")
+    cert = ACM("SSL certificate")
+
+    with Cluster("Image Processing"):
+        image_proc = [Lambda("Tachyon Lambda"),
+                      S3("Media storage"),
+                      CloudFront("Caching and CDN")]
+
+    
+    wordpress >> image_proc
+    cert >> image_proc
