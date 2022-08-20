52e111c81282d5f5c6b772ad68d8959278133402
Lee Aplin <lee@substrakt.com>
2022-08-20 20:40:29 +0100

    ADD: Media and cloudfront to web diagram

--- site.py
+++ site.py
@@ -1,6 +1,8 @@
-from diagrams import Diagram, Cluster
+from diagrams import Diagram, Cluster, Edge
 from diagrams.custom import Custom
 from diagrams.aws.database import Aurora
+from diagrams.aws.network import CloudFront
+from diagrams.aws.storage import S3
 from diagrams.saas.cdn import Cloudflare
 from diagrams.saas.logging import Papertrail
 from diagrams.onprem.inmemory import Redis
@@ -8,5 +10,25 @@ from diagrams.onprem.inmemory import Redis
 with Diagram("Website stack"):
     server = Custom("Heroku server", "./img/heroku.png")
     database = Aurora("MySQL database")
+    website_cloudfront = CloudFront("CloudFront web caching")
+    tachyon_cloudfront = CloudFront("CloudFront media caching")
+    s3 = S3("S3 media storage")
 
-    with Cluster("Heroku plugins":
+    with Cluster("Heroku plugins"):
+        plugins = [Redis("Redis cache"),
+                   Papertrail("Papertrail logging"),
+                   Custom("Process scheduler", "./img/scheduler.png")]
+
+    database \
+        >> Edge(color="purple", style="bold") \
+    << website_cloudfront \
+        >> Edge(color="purple", style="bold") \
+    << server \
+        >> Edge(color="purple", style="bold") \
+        << plugins
+
+    server \
+        >> Edge(color="purple", style="bold") \
+        << s3 \
+            >> Edge(color="purple", style="bold") \
+            << tachyon_cloudfront
