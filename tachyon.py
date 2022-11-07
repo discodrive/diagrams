7517eb7c45cec3b4e33e8ba7ade7e55a9d780bf4
Lee Aplin <lee@substrakt.com>
2022-11-07 12:14:08 +0000

    UPDATE: Tachyon diagram

--- tachyon.py
+++ tachyon.py
@@ -1,10 +1,12 @@
-from diagrams import Cluster, Diagram
+from diagrams import Cluster, Diagram, Edge
 from diagrams.custom import Custom
 from diagrams.aws.network import CloudFront
 from diagrams.aws.network import APIGateway
 from diagrams.aws.security import ACM
 from diagrams.aws.storage import S3
 from diagrams.aws.compute import Lambda
+from diagrams.aws.security import IAM
+from diagrams.aws.security import IAMRole
 
 with Diagram("Tachyon", show=False):
     wordpress = Custom("Wordpress website", "./img/wordpress.png")
@@ -16,7 +18,11 @@ with Diagram("Tachyon", show=False):
                       S3("S3 Media storage"),
                       CloudFront("CloudFront\nCaching and CDN")]
 
-    
-    wordpress >> image_proc
+    with Cluster("IAM"):
+        iam = IAM("Tachyon IAM user")
+        iam - Edge(color="brown", style="dotted") - IAMRole("Role and policy for\ninteracting with S3")
+
+    wordpress >> Edge(color="firebrick") << image_proc
     cert >> image_proc
-    image_proc >> api
+    image_proc >> Edge(color="firebrick") << api
+    image_proc >> Edge(color="darkgreen") << iam
